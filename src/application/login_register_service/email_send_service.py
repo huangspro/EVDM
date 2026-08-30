7cf61e6ff25a34966428b0cf911a0073597171c4
hhy <13612276628@163.com>
2026-08-30 19:51:59 +0800

    k

---
+++ src/application/login_register_service/email_send_service.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+
+class EmailSendService(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def send_email_of_text(self, text_content):
+        pass
--- src/application/login_register_service/login_service.py
+++ src/application/login_register_service/login_service.py
@@ -2,10 +2,13 @@ from utils.status import Status
 
 
 class LoginService:
-    def __init__(self, database):
+    def __init__(self, database, authentication_validator):
         self.database = database
+        self.authentication_validator = authentication_validator
 
     def validate_user(self, user, password):
+        self.authentication_validator()
+        
         user_from_database = self.database.find_user_by_attr("id", user.id)
         if len(user_from_database) != 1:
             return Status.USER_NOT_FOUND
