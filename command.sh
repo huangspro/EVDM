ee65a1ef5a8d6f24c960cfb63004014733c80377
hhy <13612276628@163.com>
2026-08-30 20:07:29 +0800

    auto commit

--- command.sh
+++ command.sh
@@ -1,7 +1,9 @@
+echo '```' > ProjectStructure.md
+tree -L 10 -I '__pycache__|__init__.py' >> ProjectStructure.md
+echo '```' >> ProjectStructure.md
+
 git add .
 git commit -m "auto commit"
 git push
 
-echo '```' > ProjectStructure.md
-tree -L 10 -I '__pycache__|__init__.py' >> ProjectStructure.md
-echo '```' >> ProjectStructure.md
+
